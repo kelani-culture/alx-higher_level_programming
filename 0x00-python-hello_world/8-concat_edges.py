82add3343992cbfddf478ff4e87143519ebf5b46
Null <kelanidarasimi9@gmail.com>
2023-06-05 13:03:42 -0700

    Task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.index("object-oriented"):].split()[0] + ' ' + "programming with Python"
-print(str)
+
+print(str[str.index("object-oriented"):].split()[0] + " programming with Python")
