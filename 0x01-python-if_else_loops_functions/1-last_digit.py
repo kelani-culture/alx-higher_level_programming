9eb7dea6b68277e858a4f80f24a009a9ae5933a5
Null <kelanidarasimi9@gmail.com>
2023-06-06 12:29:05 -0700

    fix pep8 and correct code

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,16 +8,15 @@ if (number > 0):
 else:
     last_digit = -(number) % 10
 
-if (last_digit > 5):
-    if (number > 0):
+if (last_digit > 5) and (number > 0):
         print(f"Last digit of {number} is {last_digit} and is greater than 5")
-    else:
-        print(f"Last digit of {number} is -{last_digit} and is greater than 5")
 
-elif (last_digit >  0) and (last_digit < 6):
+elif (last_digit > 0) and (last_digit < 6) or (number < 0):
     if (number > 0):
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last_digit} and\
+ is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is -{last_digit} and\
+ is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is {last_digit} and is 0")
