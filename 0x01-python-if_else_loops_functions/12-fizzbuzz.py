369e30f880dc22dcdc59f9d8c35461c0a7713cc0
Null <kelanidarasimi9@gmail.com>
2023-06-07 05:22:51 -0700

    Fizz buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    count = 1
+
+    while count <= 100:
+        if (count % 3 == 0 and count % 5 == 0):
+            print("FizzBuzz", end=" ")
+        elif (count % 3 == 0):
+            print("Fizz", end=" ")
+        elif (count % 5 === 0):
+            print("Buzz", end=" ")
+        else:
+            print(count, end=" ")
