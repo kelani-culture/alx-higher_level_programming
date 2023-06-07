80992228fb16c65b3a2b430c8b84b8f21c5cd1e6
Null <kelanidarasimi9@gmail.com>
2023-06-07 06:24:10 -0700

    reduce number char to 3999

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@
 
 for i in range(0, 100):
     print("{:02d}".format(i) if i <= 9 else "{:d}".format(i), end="")
-    print("," if i < 99 else "\n", end=" ")
+    print(", " if i < 99 else "\n", end="")
