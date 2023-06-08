3523d2f390d896857a8bb7462cc510d143b72c2d
Null <kelanidarasimi9@gmail.com>
2023-06-08 08:17:05 -0700

    fixing requirement error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@
 if __name__ == "__main__":
     a = 1
     b = 2
-    add = __import__("add_0").add
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b, __import__('add_0').add(a, b))))
