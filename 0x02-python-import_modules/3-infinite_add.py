f8061acb19ea328a99d4ee82383f8058003d7afb
Null <kelanidarasimi9@gmail.com>
2023-06-08 23:39:55 -0700

    fix not in main

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,12 +4,13 @@ import sys
 argvs = sys.argv
 argc = len(argvs)
 
-if (argc == 1):
-    print(0)
+if __name__ == "__main__":
+    if (argc == 1):
+        print(0)
 
-else:
-    count = 0
-    for argv in argvs[1:]:
-        int_argv = int(argv)
-        count += int_argv
-    print(count)
+    else:
+        count = 0
+        for argv in argvs[1:]:
+            int_argv = int(argv)
+            count += int_argv
+        print(count)
