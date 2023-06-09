db8c59ee4664d557121d535e7e533fd1a2a08e35
Null <kelanidarasimi9@gmail.com>
2023-06-08 23:38:31 -0700

    fix pycode style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,17 @@ import sys
 
 if __name__ == "__main__":
     argvs = sys.argv
-    len_argv = len(argvs)
+    len_argv = len(argvs) - 1
 
-    if len_argv == 1:
+    if len_argv == 0:
         print("0 arguments.")
 
     else:
         count = 1
-        print(f"{len_argv - 1} argument:" if len_argv == 2\
-else f"{len_argv - 1} arguments:")
+        if len_argv == 1:
+            print(f"{len_argv} argument:")
+        else:
+            print(f"{len_argv} arguments:")
         for argv in argvs[1:]:
             print(f"{count}: {argv}")
             count += 1
