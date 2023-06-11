dcbc935d4ab2d063e89e45654b3ec1426cb6047e
Null <kelanidarasimi9@gmail.com>
2023-06-11 10:00:48 -0700

    fix index errror

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ def new_in_list(my_list, idx, element):
 
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return my_list
 
     while count < len(my_list):
