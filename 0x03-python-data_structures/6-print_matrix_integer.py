be7e58f076fe7da0275d78123689887c82ac272f
Null <kelanidarasimi9@gmail.com>
2023-06-12 04:07:28 -0700

    fix code ouput error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         count = 0
         for j in i:
-            if count == 2:
+            if count == len(i) - 1:
                 print('{:d}'.format(j), end='')
             else:
                 print('{:d} '.format(j), end='')
