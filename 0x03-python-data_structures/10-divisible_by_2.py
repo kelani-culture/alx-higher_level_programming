7f3d42960fee8b30bd1a5c6e367b7456a9ccf784
Null <kelanidarasimi9@gmail.com>
2023-06-12 13:53:42 -0700

    fix missing white space

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,5 +2,5 @@
 
 def divisible_by_2(my_list=[]):
 
-    new_list = [True if number%2 == 0 else False for number in my_list]
+    new_list = [True if number % 2 == 0 else False for number in my_list]
     return new_list
