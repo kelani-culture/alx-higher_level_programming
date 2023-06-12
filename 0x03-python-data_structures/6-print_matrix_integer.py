3a6dd0bcaa414f1e1568a016a3b8464bfd36723b
Null <kelanidarasimi9@gmail.com>
2023-06-12 04:02:37 -0700

    fix wrong output

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,11 @@
 def print_matrix_integer(matrix=[[]]):
 
     for i in matrix:
+        count = 0
         for j in i:
-            print("{:d}".format(j), end=" ")
-
+            if count == 2:
+                print('{:d}'.format(j), end='')
+            else:
+                print('{:d} '.format(j), end='')
+            count += 1
         print()
