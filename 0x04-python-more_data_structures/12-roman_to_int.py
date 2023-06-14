e0af784bd16460e9a726435900dc5a5f139eeef8
Null <kelanidarasimi9@gmail.com>
2023-06-14 14:29:34 -0700

    made some corrections

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 from functools import reduce
 
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) == False or (roman_string == None):
+    if (roman_string == None) or len(roman_string) == 0:
         return 0
     roman_letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_num = [roman_letters.get(i) for i in roman_string]
 
-    decimal = reduce(lambda x, y: x + y if y <= x else x - y, roman_num)
+    decimal = reduce(lambda x, y: x + y if y <= x else y - x, roman_num)
     return decimal
