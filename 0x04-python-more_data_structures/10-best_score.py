967d61edef8c418c96fa6309677717473d8d5835
Null <kelanidarasimi9@gmail.com>
2023-06-14 09:24:21 -0700

    fix wrong output

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary != None else None)
+    if a_dictionary is None:
+        return a_dictionary
+    return (max(a_dictionary, key=a_dictionary.get))
