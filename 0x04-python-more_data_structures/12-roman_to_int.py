45cf400b3c21e4d2be74d50ec0fe2bdf17f29e25
Null <kelanidarasimi9@gmail.com>
2023-06-14 23:01:07 -0700

    add a readme file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     if (roman_string == None) or len(roman_string) == 0:
         return 0
     roman_letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_num = [roman_letters.get(i) for i in roman_string]
+    roman_num = [roman_letters.get(i) for i in roman_string.upper()]
 
     decimal = reduce(lambda x, y: x + y if y <= x else y - x, roman_num)
     return decimal
