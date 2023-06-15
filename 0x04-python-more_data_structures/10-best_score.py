d8b2946e8b2f21fba538748590155b0a9bd84b74
Null <kelanidarasimi9@gmail.com>
2023-06-14 23:05:35 -0700

    checke dfor len dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return a_dictionary
+    elif len(a_dictionary) == 0:
+        return None
+
     return (max(a_dictionary, key=a_dictionary.get))
