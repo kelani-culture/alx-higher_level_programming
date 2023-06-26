a51ab097f1f63a4db137bd5263227dfd79f6bdeb
Null <kelanidarasimi9@gmail.com>
2023-06-26 16:46:18 -0700

    rm white space

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 
 def safe_print_division(a, b):
     try:
-        result =  a / b
+        result = a / b
     except ZeroDivisionError:
         result = None
         print("Inside result: {}".format(result))
