9d0d6beb8aa9af45951a96009a492db279c4fd3b
Null <kelanidarasimi9@gmail.com>
2023-06-27 08:56:00 -0700

    A class square method with size 3

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+     a class Square that defines a square by
+"""
+
+class Square:
+    """
+        A class object square
+    """
+    def __init__(self, size):
+        self.__size = size
