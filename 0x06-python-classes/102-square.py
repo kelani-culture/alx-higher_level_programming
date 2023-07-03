de073fcf032a0e0065d52a990199132ff08c0809
Null <kelanidarasimi9@gmail.com>
2023-07-03 03:07:09 -0700

    checks for class equality

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,59 @@
+i#!/usr/bin/python3
+
+"""
+    check equality of a square
+"""
+
+
+class Square:
+    """
+        Checks equality of square
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def __lt__(self, other):
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return False
+
+    def __eq__(self, other):
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, Square):
+            return self.area() != other.area()
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+        return False
