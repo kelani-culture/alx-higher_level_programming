54a31287474aab5789be0407c8558a19924e88ad
Null <kelanidarasimi9@gmail.com>
2023-07-03 23:00:55 -0700

    handle width and height == 0

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
         return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        if self.__height != 0 or self.__width != 0:
-            rt = [['#' for _ in range(self.__width)] for _ in range(self.__height)]
-            return "\n".join(["".join(row) for row in rt])
-        return ""
+        if self.__height <= 0 or self.__width <= 0:
+            return ""
+        rt = [['#' for _ in range(self.__width)] for _ in range(self.__height)]
+        return "\n".join(["".join(row) for row in rt])
