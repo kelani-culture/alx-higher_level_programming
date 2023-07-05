066ed05193e6244e24063a71cb561906852df57d
Null <kelanidarasimi9@gmail.com>
2023-07-04 17:51:45 -0700

    doc test matric divider

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    A program that divides an integer array
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        A function that divides a matrix of integer
+    """
+    for list_ in matrix:
+        if isinstance(list_, list) is not True:
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            " of integers/floats")
+        for value in list_:
+            if (not isinstance(value, int)) and (not isinstance(value, float)):
+                raise TypeError("matrix must be a matrix (list of lists)"
+                                " of integers/floats")
+    for idx in range(len(matrix) - 1):
+        if len(matrix[idx]) != len(matrix[idx + 2 - 1]):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if (not isinstance(div, int)) and (not isinstance(div, float)):
+        raise TypeError("div must be a number")
+    elif (div == 0):
+        raise ZeroDivisionError("division by zero")
+
+    div = [[round(j / div, 2) for j in num] for num in matrix]
+    return div
