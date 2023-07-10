5b45b5bc5194c3a5cd3c222c91259e8c02bb0335
Null <kelanidarasimi9@gmail.com>
2023-07-10 04:46:57 -0700

    check for an instance object

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    A program
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        A functtion that returns a boolean
+    """
+    if not isinstance(obj, a_class):
+        return False
+    return True
