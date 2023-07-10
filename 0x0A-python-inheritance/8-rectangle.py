5d69db18a56a046262bcc75149f9837088c3f029
Null <kelanidarasimi9@gmail.com>
2023-07-10 09:16:57 -0700

    fix code error:

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(BaseGeometry):
         Rectangle
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
