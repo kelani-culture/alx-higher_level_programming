03b059b612b7f14ab96adb4c9a842916447e8804
Null <kelanidarasimi9@gmail.com>
2023-07-10 22:31:45 -0700

    test for sorting empty list

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""
+    Base geometry class
+"""
+
+
+class BaseGeometry:
+    """
+        Base geometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+        Rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    # calculate area of rectangle
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[{__class__.__name__}] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """
+        Square
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+
+    # calculate the area of a square
+    def area(self):
+        return super().area()
+
+    def __str__(self):
+        return super().__str__()
