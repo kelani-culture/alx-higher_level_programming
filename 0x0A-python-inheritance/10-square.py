96cd058f4dec9162ebd6a0d054effb6195328a27
Null <kelanidarasimi9@gmail.com>
2023-07-10 23:30:29 -0700

    fix checker error for bothe task 7 and 8

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,40 +1,6 @@
 #!/usr/bin/python3
-
-"""
-    Base geometry class
-"""
-
-
-class BaseGeometry:
-    """
-        Base geometry
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """
-        Rectangle
-    """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    # calculate area of rectangle
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[{__class__.__name__}] {self.__width}/{self.__height}"
+""" inherits from rectangle """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,4 +12,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
