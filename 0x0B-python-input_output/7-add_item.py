afbe3f979b9831583dc57986962c5a108b43f56a
Null <kelanidarasimi9@gmail.com>
2023-07-11 16:02:01 -0700

    fix pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,15 +7,21 @@ sys = __import__("sys")
 
 
 obj = sys.argv
+
+
 def write_to_json(obj):
+    """
+        write and load a json file
+    """
     if len(obj) == 1:
         inpt = []
         save_to_json_file(inpt, "add_item.json")
         load_from_json_file("add_item.json")
     else:
-        inpt = obj[1:] 
+        inpt = obj[1:]
         save_to_json_file(inpt, "add_item.json")
         load_from_json_file("add_item.json")
-   
+
+
 if __name__ == "__main__":
     write_to_json(obj)
