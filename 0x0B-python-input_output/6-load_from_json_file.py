03273cef2a04230406c3000de5e0aef36acf398d
Null <kelanidarasimi9@gmail.com>
2023-07-11 14:54:55 -0700

    load a json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" load a json file """
+json = __import__("json")
+
+
+def load_from_json_file(filename):
+    """ load json file """
+    
+    with open(filename) as f_json:
+        load_json = json.load(f_json)
+
+    return load_json
