b2dff606f7e760a5364d9fbcdf792dfc60d52012
Null <kelanidarasimi9@gmail.com>
2023-07-12 00:09:50 -0700

    a class Student that defines a student by

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,8 @@
 """  function that returns the dictionary description """
 
 
-
 def class_to_json(obj):
-    """ 
+    """
         function that returns the dictionary description
         obj - instance of a class
     """
