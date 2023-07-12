732dc7f976eb4e31c69aca1fe0f0aed67411e13f
Null <kelanidarasimi9@gmail.com>
2023-07-12 09:27:08 -0700

    load file to json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    A class student
+"""
+
+
+class Student:
+    """
+        A class that defines student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        student_dict = {"first_name": self.first_name,
+                        "last_name": self.last_name,
+                        "age": self.age}
+        if attrs:
+            retrieved_student = {key: student_dict[key] for key in attrs
+                                 if key in list(student_dict.keys())}
+            return retrieved_student
+        else:
+            return student_dict
+
+    def reload_from_json(self, json):
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
+        return {key: value for key, value in json.items()}
