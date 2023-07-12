7274e736a75c6684dd8c957bf0fa09db1cded0e6
Null <kelanidarasimi9@gmail.com>
2023-07-12 12:09:26 -0700

    add docstring comment and fix pycodestyle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,11 +4,15 @@
     pascal triangle
 """
 
+
 def pascal_triangle(n):
+    """
+       create pascal triangle
+    """
     if n <= 0:
         return []
+    pascal = [[1 if j == 0 else j for j in range(i)] for i in range(1, n + 1)]
 
-    pascal = [[1 if j == 0 else j for j in range (i)] for i in range(1, n + 1)]
     for i in range(1, len(pascal)):
         for j in range(1, i + 1):
             if j == len(pascal[i]) - 1:
