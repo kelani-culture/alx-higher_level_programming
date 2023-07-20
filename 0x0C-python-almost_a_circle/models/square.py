a9960a62c09706206a225a9346ffd3fe4ebc5a74
Null <kelanidarasimi9@gmail.com>
2023-07-20 08:22:40 -0700

    Circle Recycled

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,9 +20,6 @@ class Square(Rectangle):
             ValueError: If x or y is less than zero.
     """
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
-        self.x = x
-        self.y = y
         super().__init__(size, size, x, y, id)
 
     @property
