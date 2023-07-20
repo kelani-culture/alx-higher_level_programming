29c4dd38fc64a302d5e30c60edfa3b49c0233da1
Null <kelanidarasimi9@gmail.com>
2023-07-20 11:10:35 -0700

    write a test for base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    A program for testing the base class
+    Test base class
 """
 import unittest
 from models.base import Base
@@ -8,16 +8,28 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     """
-        Test alll edge cases for the base class
+        Test cases for class
     """
-    def test_base(self):
-        b_1 = Base()
-        b_2 = Base(12)
-        b_3 = Base()
-        b_4 = Base(-2)
-        b_5 = Base("I")
-        self.assertEqual(b_1.id, 1)
-        self.assertEqual(b_2.id, 12)
-        self.assertEqual(b_3.id, 2)
-        self.assertEqual(b_4.id, -2)
-        self.assertEqual(b_5.id, "I")
+    # test if input is None
+    def test_None(self):
+        B1 = Base()
+        B2 = Base()
+        B3 = Base(None)
+        self.assertEqual(B1.id, 1)
+        self.assertEqual(B2.id, 2)
+        self.assertEqual(B3.id, 3)
+
+    # test any input for id
+    def test_input(self):
+        b1 = Base(1)
+        b2 = Base("str")
+        b3 = Base([1, 3, 4])
+        b4 = Base({1})
+        b5 = Base(-1)
+        b6 = Base(float("inf"))
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, "str")
+        self.assertEqual(b3.id, [1, 3, 4])
+        self.assertEqual(b4.id, {1})
+        self.assertEqual(b5.id, -1)
+        self.assertEqual(b6.id, float("inf"))
