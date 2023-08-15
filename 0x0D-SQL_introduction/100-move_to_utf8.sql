2f11f4f68e03a015ac4dd6bfb26cf86fe3f58d64
Null <kelanidarasimi9@gmail.com>
2023-08-15 13:59:58 -0700

    Task 17 corected

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- set character to utf8mb4_unicode_ci
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
