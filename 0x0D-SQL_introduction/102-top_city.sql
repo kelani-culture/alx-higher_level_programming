1fd426b9d2c01cacf3b9319ba668fe8fde1a6efa
Null <kelanidarasimi9@gmail.com>
2023-08-16 00:40:47 +0100

    Task 103

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
---- select top 3
+-- average temperature in fahreinheit
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
+WHERE month BETWEEN 7 AND 8
 GROUP BY city
-ORDER BY avg_temp
+ORDER BY avg_temp DESC
 LIMIT 3;
