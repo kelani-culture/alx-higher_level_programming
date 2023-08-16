718703a4ecafa4938d296ffce24361a203ae25a4
Null <kelanidarasimi9@gmail.com>
2023-08-16 21:24:51 +0100

    Task 14 corrected

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
--- genre title
+-- genre by title
 SELECT tv_genres.name as name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name
