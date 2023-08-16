f2b8a32c61e4536cd57338729d47404c792f04e4
Null <kelanidarasimi9@gmail.com>
2023-08-16 15:40:26 +0100

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- use the database created
+USE hbtn_0d_usa;
+
 -- create a table
 CREATE TABLE IF NOT EXISTS states(
   id INT NOT NULL AUTO INCREMENT,
