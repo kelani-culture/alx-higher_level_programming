e44a83d6aea3cc7d0be073a8eade549ac9a41ec7
Null <kelanidarasimi9@gmail.com>
2023-08-16 17:07:54 +0100

    still not known

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,5 @@
 -- select all cities in carlifornia
-USE hbtn_0d_usa;
-
--- create database
-SELECT cities.id as id, cities.name
-FROM cities
-WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id ASC;
+SELECT id, cities
+FROM states
+WHERE name = "California"
+ORDER BY cities.id 
