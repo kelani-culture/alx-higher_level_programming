3bc044a49870e96ebd0f499ecc14490f38626314
Null <kelanidarasimi9@gmail.com>
2023-08-16 16:20:49 +0100

    task 7 corrected

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,13 +1,13 @@
--- create a table and Database
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- use the Database
+-- Use the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
--- create a table cities
-CREATE TABLE IF NOT EXISTS cities(
-  id INT NOT NULL PRIMARY KEY AUTO INCREMENT,
-  state_id INT NOT NULL,
-  name VARCHAR(256) NOT NULL,
-  FOREIGN KEY(state_id) REFERENCES states(id)
-)
+-- Create the cities table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
