e1ceae72a714a3acfbdb4b56c979b97642dfea7b
Null <kelanidarasimi9@gmail.com>
2023-08-16 22:34:10 +0100

    Task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- list all shows and genres
+SELECT
+    tv_shows.title AS title,
+    tv_genres.name AS name
+    FROM tv_shows
+    LEFT JOIN tv_show_genres
+    ON tv_show_genres.show_id = tv_shows.id
+    LEFT JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+    ORDER BY title, name;
