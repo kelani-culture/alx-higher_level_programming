128f0b47aa7ca3d01ef7e47b5b005caa5e1c8e76
Null <kelanidarasimi9@gmail.com>
2023-08-16 15:43:49 +0100

    task 6 corrected

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,7 +6,7 @@ USE hbtn_0d_usa;
 
 -- create a table
 CREATE TABLE IF NOT EXISTS states(
-  id INT NOT NULL AUTO INCREMENT,
+  id INT NOT NULL AUTO INCREMENT UNIQUE,
   name VARCHAR(256),
   PRIMARY KEY(id)
 );
