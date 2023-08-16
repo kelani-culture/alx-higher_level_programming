c0a9b98091dc8992be94048f85745fbfddcfef46
Null <kelanidarasimi9@gmail.com>
2023-08-17 00:15:54 +0100

    remove backtick

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,17 +1,17 @@
 -- list tables genres not linked to dexter
 
-SELECT DISTINCT `title`
-  FROM `tv_shows` AS `tvs`
-  LEFT JOIN `tv_show_genres` AS `tvsg`
-    ON `tvsg.show_id` = `tvs.id`
-  LEFT JOIN `tv_genres` AS `tvg` ON `tvsg.genre_id` = `tvg.id`
+SELECT DISTINCT title
+  FROM tv_shows AS tvs
+  LEFT JOIN tv_show_genres AS tvsg
+    ON tvsg.show_id = tvs.id
+  LEFT JOIN tv_genres AS tvg ON tvsg.genre_id = tvg.id
 
   WHERE title NOT IN ( SELECT title
-        FROM `tv_shows` AS `tvs`
-        LEFT JOIN `tv_show_genres` AS `tvsg`
-          ON `tvsg.show_id` = `tvs.id`
-        LEFT JOIN `tv_genres` AS `tvg` ON `tvsg.genre_id` = `tvg.id`
+        FROM tv_shows AS tvs
+        LEFT JOIN tv_show_genres AS tvsg
+          ON tvsg.show_id = tvs.id
+        LEFT JOIN tv_genres AS tvg ON tvsg.genre_id = tvg.id
  
-        WHERE `name` = "Comedy"
+        WHERE name = "Comedy"
   )
-  ORDER BY `title`
+  ORDER BY title
