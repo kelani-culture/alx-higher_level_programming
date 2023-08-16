5c18ef80490fb7ebb5fe8170cfc7098d08083b75
Null <kelanidarasimi9@gmail.com>
2023-08-16 17:13:44 +0100

    Hope it works

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- select all cities in carlifornia
 SELECT cities.id, cities.name
 FROM states, cities
-WHERE states.name = "California"
+WHERE states.name = "California" and state_id = 1
 ORDER BY cities.id 
