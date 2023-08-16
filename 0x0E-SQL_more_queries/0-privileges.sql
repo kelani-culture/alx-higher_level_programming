b3b946b3bff2a0ee46f8dbe3103dc27a353202f0
Null <kelanidarasimi9@gmail.com>
2023-08-16 12:56:22 +0100

    Task 0 corrected

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
 -- show priviledges granted to user
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
