b58eb444fd4e974d90397077854dccfb975366b5
Null <kelanidarasimi9@gmail.com>
2023-08-16 21:53:09 +0100

    Task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,10 @@
--- list genres
+-- lists of all genres
 
-SELECT tv_genres.name as genre, count(*) as number_of_shows
-  FROM tv_genres
-  LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-  GROUP BY genre
-  ORDER BY number_of_shows DESC
+SELECT
+    tv_genres.name AS genre,
+    COUNT(tv_show_genres.genre_id) AS number_of_shows
+    FROM tv_show_genres
+    INNER JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    GROUP BY genre
+    ORDER BY number_of_shows DESC;
