60cb0c8c291f83a2e9edc0917642eacd1b7af8f6
Null <kelanidarasimi9@gmail.com>
2023-08-17 00:55:54 +0100

    Task 14 corrected again last and least for now😎

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,13 +1,13 @@
 -- script that select all title dexter genre from database
 -- hbtn_0d_tvshows
 
-USE DISTINCT hbtn_0d_tvshows;
+USE hbtn_0d_tvshows;
 SELECT
-    tv_genres.name AS name
-    FROM tv_show_genres
-    INNER JOIN tv_genres
-    ON tv_show_genres.genre_id = tv_genres.id
-    INNER JOIN tv_shows
-    ON tv_shows.id = tv_show_genres.show_id
-    WHERE tv_shows.title = 'Dexter'
-    ORDER BY name;
+tv_genres.name AS name
+FROM tv_show_genres
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
