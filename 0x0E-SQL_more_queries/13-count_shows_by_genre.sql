3d9141b1d7104728084cac5b4540e494b83b6598
Null <kelanidarasimi9@gmail.com>
2023-08-16 21:10:16 +0100

    Task 14

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- list genres
-SELECT tv_genres.name as genre, count(tv_genres.id) as number_of_shows
+
+SELECT tv_genres.name as genre, count(*) as number_of_shows
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- genre title
+SELECT tv_genres.name as name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name
