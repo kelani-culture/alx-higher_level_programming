4c7ba14882544156ee730e33b137d73535383d9b
Null <kelanidarasimi9@gmail.com>
2023-08-16 21:29:11 +0100

    Task 14 resubmitted

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,11 +2,12 @@
 -- hbtn_0d_tvshows
 
 USE hbtn_0d_tvshows;
-SELECT tv_genres.name as name
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY names;
+SELECT
+    tv_genres.name AS name
+    FROM tv_show_genres
+    INNER JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+    AND tv_shows.title = 'Dexter'
+    ORDER BY name;
