2a62c5d9cdd992cf116268ed4c2ddff0cd60cea7
Null <kelanidarasimi9@gmail.com>
2023-08-16 20:54:35 +0100

    Task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- list genres
+SELECT tv_genres.name as genre, count(tv_genres.id) as number_of_shows
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL AND tv_show_genres.show_id IS NOT NULL
+GROUP BY genre
+ORDER BY number_of_shows DESC
