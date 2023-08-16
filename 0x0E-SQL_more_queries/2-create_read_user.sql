a8afebf244714c7848037ee2bdee5594851973e3
Null <kelanidarasimi9@gmail.com>
2023-08-16 13:58:16 +0100

    Task 2 corrected

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
--- a script that create a database and a user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFY BY 'user_0d_2_pwd';
+-- create database and user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- set Grant for user_0d_2
-GRANT ALL SELECT hbtn_0d_2 TO 'user_0d_2'@'localhost';
+
+-- Create the user user_0d_2 and grant privileges on the hbtn_0d_2 database
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT hbtn_0d_2.* TO 'user_0d_2'@'localhost';
