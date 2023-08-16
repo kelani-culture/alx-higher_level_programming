a29f4a980fcb5fa60c9d4cc3b35b24615f3a2a40
Null <kelanidarasimi9@gmail.com>
2023-08-16 16:36:07 +0100

    Task 8 corrected again

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
 -- select all cities in carlifornia
+USE hbtn_0d_usa;
+
+-- select all cities in carlifornia
 SELECT cities.id as id, cities.name
 FROM cities
 WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
