73f4192f1fa02e7c0da059a61a7b71e7351a3d4f
Null <kelanidarasimi9@gmail.com>
2023-08-16 22:19:49 +0100

    Task 15 remaked

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,9 @@
 -- lisst all comedy in database
-SELECT tv_shows.title as title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy"
-ORDER BY title
+SELECT
+    tv_shows.title AS title
+    FROM tv_show_genres
+    INNER JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+    AND tv_genres.name = 'Comedy' ORDER BY title;
