cffbbd742beabd8539bd6c60be3ba467ecdb39df
Null <kelanidarasimi9@gmail.com>
2023-09-11 19:07:23 +0100

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool',
+               'JavaScript is amazing']
+
+let count = 0;
+while (count < array.length) {
+  console.log(array[count]);
+  count += 1;
+}
