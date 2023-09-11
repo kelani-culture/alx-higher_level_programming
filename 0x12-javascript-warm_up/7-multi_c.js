068464b25ffc6d75392426244480ccdc5fbe8bf8
Null <kelanidarasimi9@gmail.com>
2023-09-11 20:44:10 +0100

    task 8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const arg = process.argv
+const arg = process.argv;
 if (!isNaN(arg[2])) {
   let num = parseInt(arg[2]);
   while (num > 0) {
-     console.log('C is fun');
-     num -= 1;
+    console.log('C is fun');
+    num -= 1;
   }
 }
