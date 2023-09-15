d7d652fd9c312c339d8b623a6cfbcb5f89412c81
Null <kelanidarasimi9@gmail.com>
2023-09-15 10:29:02 +0100

    task 2 resubmitted

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,7 @@ if info and len(info) == 5:
     conn = MySQLdb.connect(host="localhost", port=3306, user=info[1],
                            passwd=info[2], db=info[3])
     cur = conn.cursor()
-    state = info[4]
-    query = "SELECT * FROM states WHERE name = %s"
-    cur.execute(query, (state,))
+    cur.execute("SELECT * FROM states WHERE name = %s", (info[4],))
     row = cur.fetchall()
     for r in row:
         print(r)
