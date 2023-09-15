fb54e9e777f091fdfe206045c2e480384ea936ba
Null <kelanidarasimi9@gmail.com>
2023-09-15 10:40:04 +0100

    task 2 resubmitted again

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,19 +3,31 @@
 A script that takes in an argument and display all the
 its value if matches
 """
-import sys
 import MySQLdb
+import sys
 
-info = sys.argv
+# Get user input
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+state_name = sys.argv[4]
 
-if info and len(info) == 5:
-    conn = MySQLdb.connect(host="localhost", port=3306, user=info[1],
-                           passwd=info[2], db=info[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s", (info[4],))
-    row = cur.fetchall()
-    for r in row:
-        print(r)
+# Establish a connection
+conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                       passwd=mysql_password, db=database_name)
+cur = conn.cursor()
 
-    cur.close()
-    conn.close()
+# Define the query with a placeholder
+query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+# Execute the query with the provided value
+cur.execute(query, (state_name,))
+
+# Fetch and print the results
+results = cur.fetchall()
+for row in results:
+    print(row)
+
+# Close the cursor and connection
+cur.close()
+conn.close()
