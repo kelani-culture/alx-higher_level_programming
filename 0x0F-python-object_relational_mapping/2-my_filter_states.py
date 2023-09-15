2cc90bf899d1f838b9c59243098641caa3056209
Null <kelanidarasimi9@gmail.com>
2023-09-15 10:41:40 +0100

    task 2 resubmitted again

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,27 +7,28 @@ import MySQLdb
 import sys
 
 # Get user input
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-state_name = sys.argv[4]
+if len(sys.argv) == 5:
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-# Establish a connection
-conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                       passwd=mysql_password, db=database_name)
-cur = conn.cursor()
+    # Establish a connection
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                           passwd=mysql_password, db=database_name)
+    cur = conn.cursor()
 
-# Define the query with a placeholder
-query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    # Define the query with a placeholder
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
 
-# Execute the query with the provided value
-cur.execute(query, (state_name,))
+    # Execute the query with the provided value
+    cur.execute(query, (state_name,))
 
-# Fetch and print the results
-results = cur.fetchall()
-for row in results:
-    print(row)
+    # Fetch and print the results
+    results = cur.fetchall()
+    for row in results:
+        print(row)
 
-# Close the cursor and connection
-cur.close()
-conn.close()
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
