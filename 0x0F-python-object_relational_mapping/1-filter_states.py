6cc417ed70b8ca6180e50ad93a79f024a756963e
Null <kelanidarasimi9@gmail.com>
2023-09-16 16:16:16 +0100

    fix error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,13 +7,16 @@ import MySQLdb
 import sys
 
 info = sys.argv
-
-if info and len(info) == 4:
-    conn = MySQLdb.connect(host="localhost", port=3306, user=info[1],
-                           passwd=info[2], db=info[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
-                ORDER BY states.id")
-    state_list = cur.fetchall()
-    for state in state_list:
-        print(state)
+if __name__ == '__main__':
+    if info and len(info) == 4:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=info[1],
+                               passwd=info[2], db=info[3])
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+                    ORDER BY states.id")
+        state_list = cur.fetchall()
+        for state in state_list:
+            print(state)
+        
+        cur.close()
+        conn.close()
