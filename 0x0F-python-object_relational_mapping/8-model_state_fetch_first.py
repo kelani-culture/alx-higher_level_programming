f3730135dc3129804cce94ec1d105db593ea7245
Null <kelanidarasimi9@gmail.com>
2023-09-18 11:58:46 +0100

    task 8 add a docstring

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,6 +10,10 @@ from sys import argv
 
 
 def first_state(user, passwd, db):
+    """
+        a function that fetch the
+        first state
+    """
     try:
         engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                                format(user, passwd, db))
@@ -30,5 +34,4 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("username password database missing")
         exit(1)
-
     first_state(argv[1], argv[2], argv[3])
