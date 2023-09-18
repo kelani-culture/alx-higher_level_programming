f3f7d54029f57eafe44a3ff008934ebcb00efb5f
Null <kelanidarasimi9@gmail.com>
2023-09-18 11:54:19 +0100

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+a script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+def first_state(user, passwd, db):
+    try:
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                               format(user, passwd, db))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+    except Exception:
+        print("invalid value for user, passwd, db")
+        exit(1)
+
+    first_state = session.query(State).limit(1).all()
+
+    if first_state:
+        for state in first_state:
+            print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("username password database missing")
+        exit(1)
+
+    first_state(argv[1], argv[2], argv[3])
