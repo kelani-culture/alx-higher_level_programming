5d78e114244df5ad95e1b91a10348681887b4802
Null <kelanidarasimi9@gmail.com>
2023-11-13 20:35:58 +0100

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const arg = process.argv
+
+
+function add(a, b) {
+    console.log(parseInt(a) + parseInt(b))
+}
+
+if (arg.length <= 3 || isNaN(arg[2]) && isNaN(arg[3])) {
+    console.log(NaN)
+} else {
+    add(arg[2], arg[3])
+}
