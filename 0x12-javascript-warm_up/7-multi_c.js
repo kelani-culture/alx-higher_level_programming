52670502aba8a0ce765b9ff049bf17d0cb630113
Null <kelanidarasimi9@gmail.com>
2023-11-13 20:04:07 +0100

    Task 7 redid

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,7 @@ const arg = process.argv;
 
 if (arg.length === 2) {
   console.log('Missing number of occurrences');
-} else if (parseInt(arg[2] <= 0)) {
-  console.log();
-} else {
+} else if (parseInt(arg[2]) > 0) {
   let int = parseInt(arg[2]);
   while (int > 0) {
     console.log('C is fun');
