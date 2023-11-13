1e0d378fd3041a6b89716d8f4a1679ebcbcfb7cc
Null <kelanidarasimi9@gmail.com>
2023-11-13 17:08:37 +0100

    Task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+if (arg.length === 4) {
+  console.log(arg[2], 'is', arg[3]);
+} else if (arg.length === 3) {
+  console.log(arg[2], 'is', arg[3]);
+} else if (arg.length === 2) {
+  console.log(arg[2], 'is', arg[3]);
+}
