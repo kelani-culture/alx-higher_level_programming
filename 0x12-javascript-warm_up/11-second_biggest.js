fb1f1a2e30206d982a32c4dfc04883c60a79722d
Null <kelanidarasimi9@gmail.com>
2023-11-13 21:11:06 +0100

    Task 11 redid

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 
-function findMax (array) {
-  let max = array[0];
-  let i;
-  for (i = 0; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i];
-    }
-  }
-  return max;
+function secondMax(array) {
+  array.sort();
+  return array[array.length - 2]
 }
 
 const arg = process.argv;
@@ -17,5 +11,5 @@ if (arg.length <= 3) {
   console.log(0);
 } else {
   const array = arg.slice(2);
-  console.log(findMax(array));
+  console.log(secondMax(array));
 }
