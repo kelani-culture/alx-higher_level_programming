2605606bf0404f6ecedb0192eb59b8a526f8984d
Null <kelanidarasimi9@gmail.com>
2023-11-13 16:27:44 +0100

    Task 3 redid

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const arg =  process.argv;
-if (arg[2] == undefined) {
-    console.log('No argument');
-} else{
-    console.log(arg[2])
+const arg = process.argv;
+if (arg[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg[2]);
 }
