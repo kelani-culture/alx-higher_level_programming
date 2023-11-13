a4b52533222f71a6164e1d8d175d512587162313
Null <kelanidarasimi9@gmail.com>
2023-11-13 16:21:38 +0100

    Task 2 redid

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@
 const arg = process.argv;
 if (arg.length <= 2) {
   console.log('No argument');
+} else if (arg.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
