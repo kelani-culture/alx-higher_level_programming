a6abc5927e00eb5d64ff61057d2080b8059c4031
Null <kelanidarasimi9@gmail.com>
2023-11-14 18:12:42 +0100

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Squares = require('./5-square');
+
+class Square extends Squares {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+}
+
+module.exports = Square;
