17674ee5dbd85943c52d5fc8e412780584aa254c
Null <kelanidarasimi9@gmail.com>
2023-11-14 19:08:12 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0
-    for (element of list) {
-        if (element === searchElement) {
-            count++;
-        }
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
     }
-    return count;
-}
+  }
+  return count;
+};
