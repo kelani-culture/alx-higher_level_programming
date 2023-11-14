e936fcd0cdfe91c1e6f2ac77822f43b7b0dfb8da
Null <kelanidarasimi9@gmail.com>
2023-11-14 18:51:42 +0100

    task 6 redidss

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,9 +3,11 @@
 const Squares = require('./5-square');
 
 class Square extends Squares {
-  constructor (size){
-    this.size = size
+  constructor (size) {
+    super(size);
+    this.size = size;
   }
+
   charPrint (c = 'X') {
     for (let i = 0; i < this.size; i++) {
       for (let j = 0; j < this.size; j++) {
