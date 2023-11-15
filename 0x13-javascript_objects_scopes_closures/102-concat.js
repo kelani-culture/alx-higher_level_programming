a6a08f8c53df93fe16e3699448b9eb0917b4c82d
Null <kelanidarasimi9@gmail.com>
2023-11-15 10:11:48 +0100

    task 13 redids

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,25 +1,23 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-arg = process.argv
+const arg = process.argv;
 
 const files = arg.slice(2);
 
-function concatFile(files) {
-    
-    for (file of files) {
-        fs.readFile(file, "utf-8", (err, data) => {
-            if (err){
-                console.error(err);
-            }
-            const content = data
-            fs.appendFile(files[files.length-1], content, 'utf-8', (err) => {
-                if (err){
-                    console.error(err)
-                }
-                });
-            
-        });
-    }
+function concatFile (files) {
+  for (const file of files) {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      const content = data;
+      fs.appendFile(files[files.length - 1], content, 'utf-8', (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  }
 }
-concatFile(files)
+concatFile(files);
