73e1b87052fb7ca1a59b73917d3e7f07cdc75c5c
Null <kelanidarasimi9@gmail.com>
2023-11-17 09:58:35 +0100

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+"""
+print first stae in database
+"""
+
+arg = sys.argv
+
+if len(arg) != 4:
+    print("incomplete information to connect to database")
+    sys.exit()
+
+username, password, db_name = arg[1:4]
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    try:
+        with Session() as session:
+            states = session.query(State).filter(State.name.contains('a')).\
+                order_by(State.id).all()
+            for state in states:
+                print(f'{state.id}: {state.name}')
+    except Exception as e:
+        print("Exception:", e)
