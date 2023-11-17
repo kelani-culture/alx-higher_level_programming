8c1cfd9e52bad667448464bc48ede87025da680b
Null <kelanidarasimi9@gmail.com>
2023-11-17 13:46:25 +0100

    task 100

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Integer, Column, String
 """
 Define database models class the class fort
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.ext.declarative import declarative_base, relationship
 from sqlalchemy import Integer, Column, String
 """
 Define database models class the class fort
