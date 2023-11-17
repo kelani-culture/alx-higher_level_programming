315f155005e8cb8afb73d10dbd3791046c962008
Null <kelanidarasimi9@gmail.com>
2023-11-17 10:22:11 +0100

    task 10 redid

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,22 +5,29 @@ from model_state import Base, State
 import sys
 
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 arg = sys.argv
 
 if len(arg) != 5:
-    print('provide complete informat to connect to db')
+    print('Provide complete information to connect to the database')
     sys.exit()
 
 username, password, db_name, name = arg[1:5]
+
 if __name__ == "__main__":
     engine = create_engine(
         f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
         pool_pre_ping=True)
-    Session = sessionmaker(engine)
+    Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
+
     with Session() as session:
-        get_state = session.query(State).filter(State.name == name).\
-            first()
+        # Use a parameterized query to prevent SQL injection
+        get_state = session.query(State).filter(State.name == name).first()
 
         if get_state:
             print(get_state.id)
