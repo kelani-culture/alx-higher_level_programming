79fe18737d8b7163c799379132cbf3b767129c8d
root <null>
2023-11-30 14:16:36 -0800

    task 1 redid

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display a 200 status
-curl -s -o /dev/null "$1" -w '%{http_code}' | awk 'END {print $1 == 200 ? $0: "route 2"}'
+curl -s -L "$1" -o /dev/null -w '%{http_code}'; echo ""
