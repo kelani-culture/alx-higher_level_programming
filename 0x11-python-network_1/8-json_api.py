56494413ab515acf70e8ecbdaa7f33f970a3b51a
Null <kelanidarasimi9@gmail.com>
2023-12-01 15:44:50 -0800

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a
+letter and sends a POST request
+"""
+import sys
+import requests
+import json.decoder
+
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    params = {'q': q}
+    endpoint = "http://0.0.0.0:5000/search_user"
+
+    try:
+        response = requests.post(endpoint, params=params)
+        response.raise_for_status()
+
+        result = response.json()
+
+        if result:
+            print(f"[{result.get('id')}] {result.get('name')}")
+        else:
+            print("No result")
+
+    except json.decoder.JSONDecodeError:
+        print("Not a valid JSON")
