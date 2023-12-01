d1d3600c43938c4a9a706e445b575a552739ce1f
Null <kelanidarasimi9@gmail.com>
2023-12-01 14:15:50 +0100

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+#a script that fetches https://alx-intranet.hbtn.io/status
+url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    content = body.decode('UTF-8')
+    print(f"Body response:\n\t- type: {type(body)}")
+    print(f"\t- content: {body}\n\t- utf8 content: {content}")
