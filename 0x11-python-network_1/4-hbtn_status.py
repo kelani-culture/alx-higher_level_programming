f2bd34a3056fc2b268cdc1e1695ec4ea15c9c9fc
Null <kelanidarasimi9@gmail.com>
2023-12-01 22:58:48 +0100

    task 4 correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 
 endpoints = "https://alx-intranet.hbtn.io/status"
 request = requests.get(endpoints)
-print(f"Body Response:\n\t- type: {type(request.text)}")
+print(f"Body response:\n\t- type: {type(request.text)}")
 print(f"\t- content: {request.text}")
