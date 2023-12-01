d1df36a2c15ca92d2d9bca26e9e3cf1593404010
Null <kelanidarasimi9@gmail.com>
2023-12-01 22:42:51 +0100

    task 3

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+endpoints = "https://alx-intranet.hbtn.io/stat"
+request = requests.get('https://alx-intranet.hbtn.io/stat')
+print(request.json())
