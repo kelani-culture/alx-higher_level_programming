e39622778de6a792bd2c773b5f9ee951185ee7fc
Null <kelanidarasimi9@gmail.com>
2024-01-30 21:17:28 +0100

    send a request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(`code: ${response.status_code}`);
+});
