bf77ab37fd4db611dd35b2d9ab6397042d0e0856
Null <kelanidarasimi9@gmail.com>
2024-01-30 23:51:19 +0100

    print mmovie character

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request.get({ url: apiUrl, json: true }, (error, response, movieData) => {
+  if (error) {
+    console.error("Error fetching movie data:", error);
+    return;
+  }
+
+  // Display each character name
+  movieData.characters.forEach((characterUrl) => {
+    request.get(
+      { url: characterUrl, json: true },
+      (error, response, characterData) => {
+        if (error) {
+          console.error("Error fetching character data:", error);
+          return;
+        }
+
+        console.log(characterData.name);
+      }
+    );
+  });
+});
