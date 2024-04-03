493f7c0052c5c0cafec9366223731275ef0af3c4
Null <kelanidarasimi9@gmail.com>
2024-04-03 18:37:40 +0100

    task 13

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+/** @format */
+
+$(document).ready(function () {
+  $("#btn_translate").click(function () {
+    $.post(
+      "https://hellosalut.stefanbohacek.dev",
+      { lang: $("#language_code").val() },
+      function (data, _) {
+        $("#hello").text(data.hello);
+      }
+    );
+  });
+});
